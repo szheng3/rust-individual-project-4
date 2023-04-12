0cfb187f3c8c364a7ca27e90a25ecc3531fd153b
Sept.Griffith <sszheng3@gmail.com>
2023-04-12 14:46:57 +0000

    fixed the web

--- frontend/src/views/Classification.vue
+++ frontend/src/views/Classification.vue
@@ -52,52 +52,26 @@ const upload = async () => {
 </script>
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12"
-             sm="6">
-        <v-sheet rounded class="mt-4">
-
-          <v-card variant="flat" class="mt-4">
-            <v-card-title class="headline">Classification</v-card-title>
-            <v-form @submit.prevent="" class="pa-3">
-              <v-file-input
-                  accept="image/*"
-                  label="Upload an image"
-                  prepend-icon="mdi-camera"
-                  @change="onFileInputChange"
-              ></v-file-input>
-              <v-img
-                  max-height="500"
-                  v-if="preview"
-                  :src="preview"
-                  contain
-              ></v-img>
-              <ContentLoader v-else viewBox="0 0 400 350">
-                <rect x="0" y="13" rx="4" ry="4" width="400" height="9"/>
-                <rect x="0" y="29" rx="4" ry="4" width="100" height="8"/>
-                <rect x="0" y="50" rx="4" ry="4" width="400" height="10"/>
-                <rect x="0" y="65" rx="4" ry="4" width="400" height="10"/>
-                <rect x="0" y="79" rx="4" ry="4" width="100" height="10"/>
-                <rect x="0" y="95" rx="4" ry="4" width="400" height="10"/>
-                <rect x="0" y="110" rx="4" ry="4" width="400" height="10"/>
-                <rect x="0" y="125" rx="4" ry="4" width="100" height="10"/>
-                <rect x="0" y="140" rx="5" ry="5" width="400" height="200"/>
-              </ContentLoader>
-              <v-btn :loading="loading" type="submit" color="primary" @click="upload" class="mt-3">Submit</v-btn>
-            </v-form>
-
-          </v-card>
-        </v-sheet>
-      </v-col>
-      <v-col cols="12"
-             sm="6"
-      >
-
-
-        <v-card v-if="!(showResult )" variant="flat" class="mt-4">
-
-          <v-card-text>
-            <ContentLoader viewBox="0 0 400 350">
+    <v-row >
+      <v-col cols="12" sm="6" class="mx-auto">
+        <v-card class="mt-4 pa-4">
+          <v-card-title class="headline">Image Classification</v-card-title>
+          <v-form @submit.prevent="" class="pa-3">
+            <v-file-input
+                accept="image/*"
+                label="Upload an image"
+                prepend-icon="mdi-camera"
+                @change="onFileInputChange"
+                outlined
+            ></v-file-input>
+            <v-img
+                max-height="500"
+                v-if="preview"
+                :src="preview"
+                contain
+                class="my-3"
+            ></v-img>
+            <ContentLoader v-else viewBox="0 0 400 350">
               <rect x="0" y="13" rx="4" ry="4" width="400" height="9"/>
               <rect x="0" y="29" rx="4" ry="4" width="100" height="8"/>
               <rect x="0" y="50" rx="4" ry="4" width="400" height="10"/>
@@ -108,14 +82,24 @@ const upload = async () => {
               <rect x="0" y="125" rx="4" ry="4" width="100" height="10"/>
               <rect x="0" y="140" rx="5" ry="5" width="400" height="200"/>
             </ContentLoader>
-          </v-card-text>
+            <v-btn
+                :loading="loading"
+                type="submit"
+                color="primary"
+                @click="upload"
+                class="mt-3"
+                block
+            >
+              Submit
+            </v-btn>
+          </v-form>
         </v-card>
-        <v-card v-if="showResult " variant="flat" class="mt-4">
-          <v-card-title class="headline">Classification</v-card-title>
+      </v-col>
+      <v-col cols="12" sm="6" class="mx-auto">
+        <v-card class="mt-4 pa-4" v-if="showResult">
+          <v-card-title class="headline">Classification Result</v-card-title>
           <v-card-text>{{ result.message }}</v-card-text>
-
         </v-card>
-
       </v-col>
     </v-row>
   </v-container>
@@ -123,10 +107,16 @@ const upload = async () => {
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .v-container {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
+}
+
+.headline {
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
 }
 </style>
