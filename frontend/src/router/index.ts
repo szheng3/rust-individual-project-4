5430e27c8b1bebd05160ac4efd68ebe6e05233d4
Sept <sszheng3@gmail.com>
2023-04-11 22:04:37 -0400

    added all

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,31 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import SummarizationView from "@/views/SummarizationView.vue";
+import OnnxView from "@/views/OnnxView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      // component: HomeView
+      component: SummarizationView
+    },
+    {
+      path: '/onnx',
+      name: 'onnx',
+      // component: HomeView
+      component: OnnxView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
